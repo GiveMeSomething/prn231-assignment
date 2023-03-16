6cc8fa352fb6ca15db45ad0e9af8c61026bad832
Hoang Tien Minh <htminh.1001@gmail.com>
2023-03-16 20:26:04 +0700

    chore: init project with WebAPI + MVC

---
+++ FAPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FAPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ WebAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using WebAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Support both gRPC services and controllers
+builder.Services.AddControllers();
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
+
