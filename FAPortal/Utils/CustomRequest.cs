a4430b589500e99047ba14597a3598a8fa5aa73c
Hoang Tien Minh <htminh.1001@gmail.com>
2023-03-26 21:19:09 +0700

    feat: add get token from cookies

---
+++ FAPortal/Utils/CustomRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using FAPortal.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Utils.Jwt;
+using BusinessObject.Models;
+
+namespace FAPortal.Utils
+{
+	public static class CustomRequest
+	{
+		public static UserJwt? GetUserFromToken(this HttpRequest request)
+		{
+            if (request.Cookies.TryGetValue("jwt", out var token))
+            {
+                if (token == null)
+                {
+                    return null;
+                }
+
+                var handler = new JwtSecurityTokenHandler();
+                var claims = handler.ReadJwtToken(token).Claims;
+
+                var userId = claims.FirstOrDefault(claim => claim.Type == "UserId")?.Value;
+                var role = claims.FirstOrDefault(claim => claim.Type == "Role")?.Value;
+                if (userId == null || role == null)
+                {
+                    throw new Exception("JWT missing info. Cannot parse");
+                }
+
+                return new UserJwt
+                {
+                    UserId = Int32.Parse(userId),
+                    Role = (Role)Enum.Parse<Role>(role)
+                };
+            }
+
+            return null;
+        }
+	}
+}
+
