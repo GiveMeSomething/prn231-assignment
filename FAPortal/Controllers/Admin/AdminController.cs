7d1f11bada2cf9431f95d6626dae41bfab2f7668
Hoang Tien Minh <htminh.1001@gmail.com>
2023-03-26 23:08:10 +0700

    feat: add AdminController to view all classes and all users

--- FAPortal/Controllers/Admin/AdminController.cs
+++ FAPortal/Controllers/Admin/AdminController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FAPortal.Client;
 using Microsoft.AspNetCore.Mvc;
+using FAPortal.Utils;
+using FAPortal.DTOs;
+using BusinessObject.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +15,85 @@ namespace FAPortal.Controllers.Admin
     [Route("admin")]
     public class AdminController : Controller
     {
+        private readonly HttpHandler _handler;
+
+        public AdminController(HttpHandler handler)
+        {
+            _handler = handler;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewClasses() 
+        {
+            var token = Request.GetTokenFromCookies();
+
+            var response = await _handler.GetAsync("class", token);
+            try
+            {
+                response = response.EnsureSuccessStatusCode();
+            }catch
+            {
+                // Request failed
+                var content = await response.Content.ReadFromJsonAsync<ErrorMessage>();
+                if (content == null)
+                {
+                    ViewData["Error"] = "Login Failed";
+                }
+                else
+                {
+                    ViewData["Error"] = content.Message;
+                }
+
+                return Redirect("admin");
+            }
+
+            var classes = await response.Content.ReadFromJsonAsync<List<Class>>();
+            if(classes == null)
+            {
+                return View(new List<Class>());
+            }
+
+            return View(classes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewUsers()
+        {
+            var token = Request.GetTokenFromCookies();
+
+            var response = await _handler.GetAsync("user", token);
+            try
+            {
+                response = response.EnsureSuccessStatusCode();
+            }
+            catch
+            {
+                // Request failed
+                var content = await response.Content.ReadFromJsonAsync<ErrorMessage>();
+                if (content == null)
+                {
+                    ViewData["Error"] = "Login Failed";
+                }
+                else
+                {
+                    ViewData["Error"] = content.Message;
+                }
+
+                return Redirect("admin");
+            }
+
+            var users = await response.Content.ReadFromJsonAsync<List<User>>();
+            if (users == null)
+            {
+                return View(new List<User>());
+            }
+
+            return View(users);
         }
     }
 }
--- FAPortal/Utils/CustomRequest.cs
+++ FAPortal/Utils/CustomRequest.cs
@@ -8,36 +8,47 @@ using BusinessObject.Models;
 
 namespace FAPortal.Utils
 {
-	public static class CustomRequest
-	{
-		public static UserJwt? GetUserFromToken(this HttpRequest request)
-		{
+    public static class CustomRequest
+    {
+        public static UserJwt? GetUserFromToken(this HttpRequest request)
+        {
+            string token = request.GetTokenFromCookies();
+            if (string.IsNullOrEmpty(token))
+            {
+                return null;
+            }
+
+            var handler = new JwtSecurityTokenHandler();
+            var claims = handler.ReadJwtToken(token).Claims;
+
+            var userId = claims.FirstOrDefault(claim => claim.Type == "UserId")?.Value;
+            var role = claims.FirstOrDefault(claim => claim.Type == "Role")?.Value;
+            if (userId == null || role == null)
+            {
+                throw new Exception("JWT missing info. Cannot parse");
+            }
+
+            return new UserJwt
+            {
+                UserId = Int32.Parse(userId),
+                Role = (Role)Enum.Parse<Role>(role)
+            };
+        }
+
+        public static string GetTokenFromCookies(this HttpRequest request)
+        {
             if (request.Cookies.TryGetValue("jwt", out var token))
             {
                 if (token == null)
                 {
-                    return null;
+                    return "";
                 }
 
-                var handler = new JwtSecurityTokenHandler();
-                var claims = handler.ReadJwtToken(token).Claims;
-
-                var userId = claims.FirstOrDefault(claim => claim.Type == "UserId")?.Value;
-                var role = claims.FirstOrDefault(claim => claim.Type == "Role")?.Value;
-                if (userId == null || role == null)
-                {
-                    throw new Exception("JWT missing info. Cannot parse");
-                }
-
-                return new UserJwt
-                {
-                    UserId = Int32.Parse(userId),
-                    Role = (Role)Enum.Parse<Role>(role)
-                };
+                return token;
             }
 
-            return null;
+            return "";
         }
-	}
+    }
 }
 
